93c27a9398e7057904f13a47f9f16950dabb65b2
sillypoise <somewhatjose@gmail.com>
2024-11-01 10:48:45 -0500

    format correctly

--- roles/nix/files/sillypoise/home.nix
+++ roles/nix/files/sillypoise/home.nix
@@ -1,40 +1,38 @@
-{ config, pkgs, ... }:
-
-# Enable Home Manager
-home.stateVersion = "24.05"; # Change this to the appropriate version if needed
-
+# { config, pkgs, ... }:
 {
-  # Package Management
-  home.packages = with pkgs; [
-    neovim
-    ripgrep
-    fzf
-    bat
-    broot
-    fd
-    eza
-    gnused
-    sd
-    lazygit
-    lazydocker
-    jq
-    tree-sitter
-    starship
-    btop
-    gh
-    tailscale
-    git
-    zellij
-    sqlite-utils
-    datasette
-    rustup
-    rsync
-    awscli2
-    aws-vault
-    delta
-    volta
-    zoxide
-  ];
+    # Enable Home Manager
+    home.stateVersion = "24.05"; # Change this to the appropriate version if needed
+    # Package Management
+    home.packages = with pkgs; [
+        neovim
+        ripgrep
+        fzf
+        bat
+        broot
+        fd
+        eza
+        gnused
+        sd
+        lazygit
+        lazydocker
+        jq
+        tree-sitter
+        starship
+        btop
+        gh
+        tailscale
+        git
+        zellij
+        sqlite-utils
+        datasette
+        rustup
+        rsync
+        awscli2
+        aws-vault
+        delta
+        volta
+        zoxide
+    ];
 }
 
 
