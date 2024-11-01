08122e7d81e3895173abbd960422ddecf922aab1
sillypoise <somewhatjose@gmail.com>
2024-11-01 10:42:41 -0500

    add packages

---
+++ roles/nix/files/sillypoise/home.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+
+{
+  # Package Management
+  home.packages = with pkgs; [
+    neovim
+    ripgrep
+    fzf
+    bat
+    broot
+    fd
+    eza
+    gnused
+    sd
+    lazygit
+    lazydocker
+    jq
+    tree-sitter
+    starship
+    btop
+    gh
+    tailscale
+    git
+    zellij
+    sqlite-utils
+    datasette
+    rustup
+    rsync
+    awscli2
+    aws-vault
+    delta
+    volta
+    zoxide
+  ];
+}
+
+
