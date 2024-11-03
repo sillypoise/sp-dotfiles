c0dd3007ce3b4e108de829ba59136db09fa60adc
sillypoise <somewhatjose@gmail.com>
2024-11-03 12:13:44 -0500

    add aliases

--- roles/zsh/files/zsh/aliases.zsh
+++ roles/zsh/files/zsh/aliases.zsh
@@ -1,2 +1,67 @@
-alias reload="source ~/.zshrc"
+# Aliases
+alias ..='cd ..'
+alias ...='cd ../../../'
+alias ....='cd ../../../../'
 alias c='clear'
+alias cp='cp -iv'
+alias grep="rg"
+alias cat="bat"
+alias chmod="chmod -c"
+alias chown="chown -c"
+alias find="fd"
+alias lg="lazygit"
+alias ld="lazydocker"
+alias ls="exa"
+alias ll="exa -l"
+alias sed="sd"
+alias extip='curl https://myip.wtf/text'
+alias extipjson='curl https://myip.wtf/json'
+alias mkdir='mkdir -pv'
+alias mv='mv -iv'
+alias ports='netstat -tulanp'
+alias rm='rm -iv'
+alias rmdir='rmdir -v'
+alias sqsh="squoosh-cli"
+alias pnx="pnpm nx"
+# alias speedtest='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -'
+alias vim="nvim"
+alias svim='sudo vim'
+alias reload="source ~/.zshrc"
+alias zj="zellij"
+alias zshrc="nvim ~/.zshrc"
+alias fjson="pbpaste | jq '.' | pbcopy"
+alias watch='watch -d'
+alias weather='curl wttr.in'
+alias wget='wget -c'
+
+# SSH
+alias sshg='eval $(ssh-agent) && ssh-add ~/.ssh/gh_id_ed25519'
+alias sshgv='eval $(ssh-agent) && ssh-add ~/.ssh/vanta_gh_id_ed25519'
+alias ssha='eval $(ssh-agent) && ssh-add ~/.ssh/aws_id_ed25519'
+alias sshsp='eval $(ssh-agent) && ssh-add ~/.ssh/sp_work_ed25519'
+
+## Python aliases
+alias py="python3"
+alias pvenv='python3 -m venv venv && ln -s venv/bin/activate .activate.sh && echo "deactivate" > .deactivate.sh'
+
+## SQLite
+alias sqlite="sqlite3"
+alias sqlu="sqlite-utils"
+
+# git aliases
+alias g="git"
+alias gs="git status"
+alias ga="git add"
+alias gci="git commit"
+alias gP="git push"
+alias gf="git fetch"
+alias gp="git pull"
+alias gpr="git pull --rebase"
+alias gco="git checkout"
+
+# GH & Copilot CLI 
+alias ghas="gh auth switch"
+eval "$(github-copilot-cli alias -- "$0")"
+
+# Remote Development Servers
+alias rds-sp="ssh sillypoise@sp-dev"
