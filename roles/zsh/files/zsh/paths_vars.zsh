5c541c04c25ff2ed58af3d1494b1d4825de9dd4e
sillypoise <somewhatjose@gmail.com>
2024-11-03 12:50:25 -0500

    fix paths

--- roles/zsh/files/zsh/paths_vars.zsh
+++ roles/zsh/files/zsh/paths_vars.zsh
@@ -5,12 +5,17 @@ addToPath "$HOME/.dotfiles/bin"
 addToPath "$HOME/.local/bin"
 
 # Add Nix to PATH if available
-sourceIfExists "$HOME/.nix-profile/etc/profile.d/nix.sh"
+sourceIfExists "/etc/profile.d/nix.sh"
 
 # Volta setup
 if [ -d "$HOME/.volta" ]; then
     export VOLTA_HOME="$HOME/.volta"
     addToPathFront "$VOLTA_HOME/bin"
+fi
+
+# Github Copilot CLI Prompt setup
+if command -v github-copilot-cli &>/dev/null; then
+    eval "$(github-copilot-cli alias -- "$0")"
 fi
 
 # Rustup setup
@@ -27,9 +32,6 @@ fi
 if command -v zoxide &>/dev/null; then
     eval "$(zoxide init zsh --cmd cd)"
 fi
-
-# Broot shell integration
-sourceIfExists "$HOME/.config/broot/launcher/bash/br"
 
 # Optional tooling setup (e.g., Aactivator)
 if command -v aactivator &>/dev/null; then
