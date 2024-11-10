327cd891fb8b16a912002109ca4965dfd1d5f497
sillypoise <somewhatjose@gmail.com>
2024-11-10 13:15:28 -0500

    testing installing packages

--- roles/nix/files/sillypoise/home.nix
+++ roles/nix/files/sillypoise/home.nix
@@ -10,35 +10,35 @@
 
     # Package Management
     home.packages = with pkgs; [
-        neovim
-        ripgrep
-        fzf
+        # neovim
+        # ripgrep
+        # fzf
         bat
-        broot
-        fd
-        eza
-        gnused
-        sd
-        lazygit
-        lazydocker
-        jq
-        tree-sitter
-        starship
-        btop
-        gh
-        # tailscale
-        git
-        zellij
-        sqlite-utils
-        datasette
-        rustup
-        rsync
-        awscli2
-        aws-vault
-        delta
-        volta
+        # broot
+        # fd
+        # eza
+        # gnused
+        # sd
+        # lazygit
+        # lazydocker
+        # jq
+        # tree-sitter
+        # starship
+        # btop
+        # gh
+        tailscale
+        # git
+        # zellij
+        # sqlite-utils
+        # datasette
+        # rustup
+        # rsync
+        # awscli2
+        # aws-vault
+        # delta
+        # volta
         # ufw
-        zoxide
+        # zoxide
     ];
 }
 
